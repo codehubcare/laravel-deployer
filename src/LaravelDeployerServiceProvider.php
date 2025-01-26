5467a61bad7725e27a2bedb767d364bc319b0d62
Shamshad Zaheer <shamshadtech101@gmail.com>
2025-01-25 21:37:35 -0800

    empty folders

---
+++ src/LaravelDeployerServiceProvider.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Codehubcare\LaravelDeployer;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelDeployerServiceProvider extends ServiceProvider
+{
+    public function register() {}
+}
