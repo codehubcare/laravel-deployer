bd588c74b6441bcf0f9de70973518f4e09cd48a5
Shamshad Zaheer <shamshadtech101@gmail.com>
2025-02-21 21:12:18 -0800

    Add configuration and view files for Laravel Deployer package

--- src/Http/Controllers/DeployController.php
+++ src/Http/Controllers/DeployController.php
@@ -13,7 +13,7 @@ class DeployController extends Controller
      */
     public function index()
     {
-        return view('index');
+        return view('laravel-deployer::index');
     }
 
 
--- src/LaravelDeployerServiceProvider.php
+++ src/LaravelDeployerServiceProvider.php
@@ -11,7 +11,10 @@ class LaravelDeployerServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function register() {}
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__ . '/config/laravel-deployer.php', 'laravel-deployer');
+    }
 
     /**
      * Bootstrap any application services.
@@ -20,16 +23,24 @@ class LaravelDeployerServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // Load the routes
+        // Load routes, views, migrations, and config
         $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
+        $this->loadViewsFrom(__DIR__ . '/resources/views', 'laravel-deployer');
+        $this->mergeConfigFrom(__DIR__ . '/config/laravel-deployer.php', 'laravel-deployer');
 
-        // Publish the configuration file
-        $this->publishes([
-            __DIR__ . '/../config/laravel-deployer.php' => config_path('laravel-deployer.php'),
-        ]);
+        // Optionally publish views and config
+        $this->publishes(
+            [
+                __DIR__ . '/resources/views' => resource_path('views/vendor/laravel-deployer'),
+            ],
+            'laravel-deployer-views',
+        );
 
-        // Load the views
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'laravel-deployer');
-
+        $this->publishes(
+            [
+                __DIR__ . '/config/laravel-deployer.php' => config_path('laravel-deployer.php'),
+            ],
+            'laravel-deployer-config',
+        );
     }
 }
